4e7b072e8817b5f0947a172e73de0d20142c585d
Alex Shyshkov <shi-alex@ukr.net>
2018-08-07 21:38:26 +0300

    Close #3

--- MusicViewer/MusicFileProperties/Genre.cs
+++ MusicViewer/MusicFileProperties/Genre.cs
@@ -2,7 +2,7 @@
 
 namespace MusicViewer.MusicFileProperties
 {
-    class Genre : FileItem
+    internal class Genre : FileItem
     {
         public ICollection<Genre> Genres { get; } = new List<Genre>();
     }
